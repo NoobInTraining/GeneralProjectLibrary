e65e4beb55632ee2c9939f4159ce80982495714c
Noobi <nan@gmail.com>
2017-06-18 22:00:46 +0200

    Added Mathfunction and some base logic for WPF

---
+++ GeneralProjectLibrary/MathFunctions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GeneralProjectLibrary
+{
+    static class MathFunctions
+    {
+        /// <summary>
+        /// Tries to pass an input string to an integer value using regex. Returns 0 if the resulting Regex is an empty string.
+        /// </summary>
+        /// <param name="value">The string to be converted</param>
+        /// <returns>The integer value of the input</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="OverflowException">If the input value is over the size of an integer.</exception>
+        static public int StringToInt(string value)
+        {
+            //check if null
+            if (value == null)
+                throw new ArgumentNullException("The input value is not allowed to be null-");
+
+            try
+            {
+                return int.Parse(value);
+            }
+            catch (FormatException)
+            {
+                //replace all non-numbers and check if it's an empty string
+                var number = Regex.Replace(value, "\\D+", "");
+                return int.Parse((number == "")? "0":number);
+            }
+        }
+    }
+}
